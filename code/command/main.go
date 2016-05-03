c705a99b4001f7bcd00aa8921927951bb7fa6de2
mass10 <irisawa.masaru@gmail.com>
2016-05-03 19:35:02 +0900

    wip

--- code/command/main.go
+++ code/command/main.go
@@ -3,18 +3,15 @@ package main
 import "fmt"
 import "os/exec"
 import "log"
-import "bytes"
 
 func main() {
 
 	command := exec.Command("ls", "-lF", "/tmp")
-	var buffer bytes.Buffer
-	command.Stdout = &buffer
-	error := command.Run()
+	out, error := command.Output()
 	if error != nil {
 		log.Fatal(error)
 		return
 	}
-	fmt.Println(buffer.String())
+	fmt.Println(string(out))
 }
 
