5cbbce53f5d8720d2935d0864e3af571f83e6527
mass10 <irisawa.masaru@gmail.com>
2016-05-05 09:02:33 +0900

    wip

--- code/networking/http/http.go
+++ code/networking/http/http.go
@@ -7,15 +7,16 @@ import (
 )
  
 func main() {
-	response, err := http.Get("http://www.google.co.jp")
+	response, err := http.Get("http://www.yahoo.co.jp")
 	if err != nil {
+		fmt.Printf("[ERROR] %s\n", err)
 		return
 	}
 	defer response.Body.Close()
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
+		fmt.Printf("[ERROR] %s\n", err)
 		return
 	}
 	fmt.Println(string(body))
 }
-
