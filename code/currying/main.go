d3fe339f5cf8a89b2bce39f41e595244926de5e3
mass10 <irisawa.masaru@gmail.com>
2016-05-07 21:06:58 +0900

    wip

--- code/currying/main.go
+++ code/currying/main.go
@@ -3,7 +3,7 @@ package main
 import "os"
 import "fmt"
 
-func get_logger(out *os.File) func(message string) {
+func getLogger(out *os.File) func(message string) {
 
 	return func(message string) {
 		fmt.Fprintf(out, "%s\n", message)
@@ -12,9 +12,8 @@ func get_logger(out *os.File) func(message string) {
 
 func main() {
 
-	logger := get_logger(os.Stdout)
-
-	logger("hello")
-	logger("hello")
-	logger("hello")
+	log := getLogger(os.Stdout)
+	log("hello 1")
+	log("hello 2")
+	log("hello 3")
 }
