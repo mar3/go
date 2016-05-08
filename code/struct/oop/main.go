825656a584d06a6a884a64ef0f9258204e78ff52
mass10 <irisawa.masaru@gmail.com>
2016-05-08 16:15:37 +0900

    wip

--- code/struct/oop/main.go
+++ code/struct/oop/main.go
@@ -9,11 +9,18 @@ type User struct {
 }
 
 func (this *User) toJSON() string {
-	return fmt.Sprintf("{id:%v, name:\"%v\", email:\"%v\"}",
+
+	return fmt.Sprintf(
+		"{id:%v, name:\"%v\", email:\"%v\"}",
 		this.id, this.name, this.email)
 }
 
 func main() {
-	user := User{id: 1, name: "Max Middleton", email: "david.max.middleton@gmail.com"}
+
+	user := User{
+		id: 12345,
+		name: "Max Middleton",
+		email: "david.m.middleton@gmail.com"}
+
 	fmt.Println(user.toJSON())
 }
