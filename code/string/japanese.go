92d36df0833b1a589c41eeca3ea6304d1b44d077
mass10 <mass10.github@gmail.com>
2017-01-20 00:13:24 +0900

    wip

---
+++ code/string/japanese.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func mid(s string, n int) string {
+
+	return ""
+}
+
+func test(s string) {
+
+	fmt.Printf("length: %v\n", len([]rune(s)))
+}
+
+func main() {
+
+	test("日本語")
+}
