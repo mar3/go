c67323d3c8ddd9231fcbaf0f12926ab4f31dded5
mass10 <mass10.github@gmail.com>
2018-04-11 22:57:10 +0900

    wi

--- code/randomize/randomize.go
+++ code/randomize/randomize.go
@@ -6,38 +6,28 @@ import "github.com/google/uuid"
 import "io/ioutil"
 import "path/filepath"
 
-func isDir(name string) bool {
-
-	info, err := os.Stat(name)
-	if err != nil {
-		return false
-	}
-	return info.IsDir()
-}
-
 func generateNewName() (string) {
-
 	id := uuid.New()
 	return fmt.Sprintf("%v", id)
 }
 
 func rename(path string) {
-
 	parent := filepath.Dir(path)
-	// name := filepath.Base(path)
 	ext := filepath.Ext(path)
-
 	newpath := filepath.Join(parent, generateNewName() + ext)
-	fmt.Printf("rename ... [%v] >> [%v]\n", path, newpath)
+	fmt.Printf("rename ... [%v] >> [%v]", path, newpath)
+	fmt.Println()
 	os.Rename(path, newpath)
 }
 
 func run(s string, operation func(string)) {
-
-	if s == "" {
+	info, err := os.Stat(s)
+	if err != nil {
+		fmt.Printf("[ERROR] %v", err)
+		fmt.Println()
 		return
 	}
-	if isDir(s) {
+	if info.IsDir() {
 		entries, _ := ioutil.ReadDir(s)
 		for _, f := range entries {
 			path := filepath.Join(s, f.Name())
@@ -49,7 +39,6 @@ func run(s string, operation func(string)) {
 }
 
 func main() {
-
 	for _, e := range(os.Args[1:]) {
 		run(e, rename)
 	}
