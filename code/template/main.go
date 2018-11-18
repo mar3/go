9f164f552fd501c2ba4b2a2f7ea673e39833db60
mass10 <mass10.github@gmail.com>
2018-11-18 14:04:25 +0900

    wip

--- code/template/main.go
+++ code/template/main.go
@@ -1,12 +1,31 @@
 package main
 
 import "os"
+import "fmt"
+import "bytes"
 import "text/template"
 
-func main() {
+func test1() {
 	t, _ := template.ParseFiles("template.txt")
 	content := make(map[string]string)
 	content["url"] = "https://yukan-club.xyz/activate/08eheh392h2e9y32jhw29eyhas821h3382th"
 	content["you"] = "オバマ"
+	content["undefined1"] = "オバマ"
 	t.Execute(os.Stdout, content)
+}
+
+func test2() {
+	t, _ := template.ParseFiles("template.txt")
+	content := make(map[string]string)
+	content["url"] = "https://yukan-club.xyz/activate/08eheh392h2e9y32jhw29eyhas821h3382th"
+	content["you"] = "オバマ"
+	content["undefined1"] = "オバマ"
+	var buffer bytes.Buffer
+	t.Execute(&buffer, content)
+	fmt.Println(string(buffer.Bytes()))
+}
+
+func main() {
+	test1();
+	test2();
 }
