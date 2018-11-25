868cbc218f8f986b46f2972b7f4c0ef3f53777f4
mass10 <mass10.github@gmail.com>
2018-11-25 11:55:45 +0900

    wip

--- code/template/main.go
+++ code/template/main.go
@@ -7,21 +7,21 @@ import "text/template"
 
 func test1() {
 	t, _ := template.ParseFiles("template.txt")
-	content := make(map[string]string)
-	content["url"] = "https://yukan-club.xyz/activate/08eheh392h2e9y32jhw29eyhas821h3382th"
-	content["you"] = "オバマ"
-	content["undefined1"] = "オバマ"
-	t.Execute(os.Stdout, content)
+	fields := make(map[string]string)
+	fields["url"] = "https://yukan-club.xyz/activate/08eheh392h2e9y32jhw29eyhas821h3382th"
+	fields["you"] = "オバマ"
+	fields["undefined1"] = "オバマ"
+	t.Execute(os.Stdout, fields)
 }
 
 func test2() {
 	t, _ := template.ParseFiles("template.txt")
-	content := make(map[string]string)
-	content["url"] = "https://yukan-club.xyz/activate/08eheh392h2e9y32jhw29eyhas821h3382th"
-	content["you"] = "オバマ"
-	content["undefined1"] = "オバマ"
+	fields := make(map[string]string)
+	fields["url"] = "https://yukan-club.xyz/activate/08eheh392h2e9y32jhw29eyhas821h3382th"
+	fields["you"] = "オバマ"
+	fields["undefined1"] = "オバマ"
 	var buffer bytes.Buffer
-	t.Execute(&buffer, content)
+	t.Execute(&buffer, fields)
 	fmt.Println(string(buffer.Bytes()))
 }
 
