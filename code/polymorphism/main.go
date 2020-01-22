9d1e8d63accae7ac7cd7537b894cb4f9bc435626
Irisawa Masaru <mass10.github@gmail.com>
2020-01-23 00:20:25 +0900

    Create main.go

---
+++ code/polymorphism/main.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+// バッチ処理基底クラス
+type AbstractBatch interface {
+	Invoke()
+}
+
+// Batch1: XXX CSV 登録処理
+type Batch1 struct {
+}
+
+func (this *Batch1) Invoke() {
+	fmt.Println("[TARCE] <Batch1.Invoke()> !")
+}
+
+// コンフィギュレーションクラス
+type Configuration struct {
+}
+
+func (this *Configuration) LoadBatchInstance(id string) AbstractBatch {
+	switch id {
+	case "Batch1":
+		batch := Batch1{}
+		return &batch
+	}
+	return nil
+}
+
+// タスク管理
+type TaskManager struct {
+}
+
+func (this *TaskManager) Execute(batch AbstractBatch) {
+
+	// バッチ処理オブジェクトを初期化します。
+	if batch == nil {
+		fmt.Println("[WARN] batch is nil.")
+		return
+	}
+	// バッチ処理を呼び出します。
+	batch.Invoke()
+}
+
+// アプリケーションクラス
+type Application struct {
+}
+
+func (this *Application) Run() {
+
+	fmt.Println("### START ###")
+	conf := Configuration{}
+	batch := conf.LoadBatchInstance("Batch1")
+	runner := TaskManager{}
+	runner.Execute(batch)
+	fmt.Println("Ok.")
+	fmt.Println("--- END ---")
+}
+
+// エントリーポイント
+func main() {
+
+	app := Application{}
+	app.Run()
+}
