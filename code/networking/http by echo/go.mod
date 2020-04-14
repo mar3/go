f7309085c06eeb6f5a37b066434d67a7ff8ff96c
mass10 <mass10.github@gmail.com>
2020-04-14 16:09:51 +0900

    wip

---
+++ code/networking/http by echo/go.mod
@@ -0,0 +1,10 @@
+module echoweb1
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
+)
--- code/networking/http by echo/main.go
+++ code/networking/http by echo/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "net/http"
-import "github.com/labstack/echo"
-import "text/template"
-import "bytes"
+import (
+	"bytes"
+	"net/http"
+	"text/template"
 
-func default_handler(ctx echo.Context) error {
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func default_handler(c echo.Context) error {
 
 	t, _ := template.ParseFiles("templates/index.html")
 	content := make(map[string]string)
@@ -13,16 +17,29 @@ func default_handler(ctx echo.Context) error {
 	content["you"] = "オバマ"
 	buffer := new(bytes.Buffer)
 	t.Execute(buffer, content)
-	return ctx.String(http.StatusOK, string(buffer.Bytes()))
-	// return ctx.String(http.StatusOK, "Hello, World!")
+	return c.HTML(http.StatusOK, string(buffer.Bytes()))
+}
+
+func hello1_handler(c echo.Context) error {
+
+	return c.String(http.StatusOK, "Hello, World!")
 }
 
 func main() {
 
 	e := echo.New()
 
+	// middleware
+	// e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	e.Use(middleware.Recover())
+
 	// routing
 	e.GET("/", default_handler)
+	e.GET("/hello1", hello1_handler)
 
 	// listenning
 	e.Logger.Fatal(e.Start(":8081"))
