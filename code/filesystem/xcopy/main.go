8e0ad7de7a4760c857e0c4e3d45206b60253b34e
Irisawa Masaru <mass10.github@gmail.com>
2020-04-18 00:31:09 +0900

    wip

--- code/filesystem/xcopy/main.go
+++ code/filesystem/xcopy/main.go
@@ -21,6 +21,7 @@ func copyFile(left string, right string) error {
 		return error
 	}
 	_, error = io.Copy(leftFile, rightFile)
+	// TODO: タイムスタンプの貼り付け
 	if false {
 		mtime := time.Date(2006, time.February, 1, 3, 4, 5, 0, time.UTC)
 		atime := time.Date(2007, time.March, 2, 4, 5, 6, 0, time.UTC)
@@ -73,7 +74,6 @@ func xcopy(sourcePath string, destinationPath string) error {
 			dest := path.Join(destinationPath, source.Name())
 			return copyFile(src, dest)
 		}
-		// sourceFullPath := path.Join(sourcePath, source.Name())
 		return copyFile(sourcePath, destinationPath)
 	}
 }
