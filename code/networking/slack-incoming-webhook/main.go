a25f217b9c7e59f8c4bf4175c13097372cbc3292
Irisawa Masaru <mass10.github@gmail.com>
2020-09-25 21:35:24 +0900

    wip

--- code/networking/slack-incoming-webhook/main.go
+++ code/networking/slack-incoming-webhook/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -17,7 +21,8 @@ func getCurrentTimestamp() string {
 }
 
 type Configuration struct {
-	WebhookUrl string
+	WebhookUrl  string
+	AccessToken string
 }
 
 // コンフィギュレーション
@@ -34,6 +39,7 @@ func (self *Configuration) Configure() bool {
 		panic(err)
 	}
 	self.WebhookUrl = fmt.Sprintf("%v", dat["webhook_url"])
+	self.AccessToken = fmt.Sprintf("%v", dat["access_token"])
 
 	return true
 }
@@ -79,18 +85,18 @@ func get(url string) string {
 }
 
 type Application struct {
-	_conf *Configuration
+	conf *Configuration
 }
 
 func (self *Application) Configure() *Configuration {
 
-	if self._conf != nil {
-		return self._conf
+	if self.conf != nil {
+		return self.conf
 	}
 
-	self._conf = &Configuration{}
-	self._conf.Configure()
-	return self._conf
+	self.conf = &Configuration{}
+	self.conf.Configure()
+	return self.conf
 }
 
 func (self *Application) PostSlackWebhook(content map[string]string) string {
@@ -125,18 +131,113 @@ func (self *Application) PostSlackWebhook(content map[string]string) string {
 	return string(body)
 }
 
+func (self *Application) UploadFile(channel string, path string) string {
+
+	//curl \
+	//  -F file=@cycling.jpeg \
+	//  -F "initial_comment=Hello, Leadville" \
+	//  -F channels=C0R7MFNJD \
+	//  -H "Authorization: Bearer xoxp-123456789" \
+	//  https://slack.com/api/files.upload
+
+	conf := self.Configure()
+	if conf == nil {
+		return ""
+	}
+
+	if false {
+		content := make(map[string]string)
+		contentText, err := json.Marshal(content)
+		if err != nil {
+			fmt.Printf("[ERROR] コンテンツの加工に失敗しています。理由: [%s]", err.Error())
+			return ""
+		}
+		fmt.Println(contentText)
+	}
+
+	// Slack Bot に与えられたアクセストークン
+	accessTokenHeader := fmt.Sprintf("Bearer %s", conf.AccessToken)
+	fmt.Printf("[TRACE] accessToken: [%s]\n", accessTokenHeader)
+
+	// values := url.Values{}
+
+	// 送信用バッファ
+	var buf bytes.Buffer
+
+	// マルチパート書き込み用オブジェクトを作成
+	multipartWriter := multipart.NewWriter(&buf)
+
+	// file フィールド
+	{
+		// ファイルをオープン
+		f, err := os.Open(path)
+		if err != nil {
+			return ""
+		}
+		defer f.Close()
+		streamWriter, error := multipartWriter.CreateFormFile("file", "main.go")
+		if error != nil {
+			return ""
+		}
+		if _, error = io.Copy(streamWriter, f); error != nil {
+			return ""
+		}
+	}
+
+	// フィールド:
+	{
+		multipartWriter.WriteField("initial_comment", "さあうけとるがよい")
+	}
+
+	// フィールド: channels
+	if true {
+		multipartWriter.WriteField("channels", "#notifications")
+	}
+
+	multipartWriter.Close()
+
+	url := "https://slack.com/api/files.upload"
+
+	// POST
+	client := getTlsClient()
+	req, _ := http.NewRequest("POST", url, &buf)
+	fmt.Printf("[TRACE] URL: [%s]\n", url)
+	fmt.Printf("[TRACE] Content-Type: [%s]\n", multipartWriter.FormDataContentType())
+	req.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+	req.Header.Set("Authorization", accessTokenHeader)
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("[ERROR] リクエストを完了できません。理由: [%s]\n", err)
+		return ""
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("[ERROR] コンテンツの読み込みに失敗しています。理由: [%s]\n", err.Error())
+		return ""
+	}
+
+	return string(body)
+}
+
 // アクセストークンを使用して GitHub のファイルを DELETE します。
 func (self *Application) Run() {
 
-	content := make(map[string]string)
-	content["text"] = "はじめまして ようこそ おじゃまいたします さようなら ごきげんよう"
-
-	response := self.PostSlackWebhook(content)
-	if response == "" {
-		return
+	if false {
+		content := make(map[string]string)
+		content["text"] = "はじめまして ようこそ おじゃまいたします さようなら ごきげんよう"
+		response := self.PostSlackWebhook(content)
+		if response == "" {
+			return
+		}
+		fmt.Println(response)
 	}
 
-	fmt.Println(response)
+	if true {
+		// response := self.UploadFile("CNH0XTKJ7", "main.go")
+		response := self.UploadFile("notifications", "main.go")
+		fmt.Printf("[TRACE] RESPONSE: [%s]\n", response)
+	}
 }
 
 func main() {
